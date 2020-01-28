f81719b0f381990158f7667b0ba65431bae18a26
Tyler Jenkins <tyjenki@gmail.com>
2020-01-27 22:43:04 -0500

    got prompts working and template literal HTML takes user responses and inserts them into dynamically generated index.html file

---
+++ index.js
@@ -0,0 +1,105 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?"
+    },
+    {
+      type: "input",
+      name: "location",
+      message: "Where are you from?"
+    },
+    {
+      type: "input",
+      name: "color",
+      message: "What is your favorite color?"
+    },
+    {
+      type: "input",
+      name: "hobby",
+      message: "What is your favorite hobby?"
+    },
+    {
+      type: "input",
+      name: "game",
+      message: "What is your favorite game?"
+    },
+    {
+      type: "input",
+      name: "movie",
+      message: "Name your favorite movie"
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username"
+    },
+    {
+      type: "input",
+      name: "linkedin",
+      message: "Enter your LinkedIn URL."
+    }
+  ]);
+}
+
+function createHTML(answers) {
+  return `
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+  <title>Dev Profile</title>
+  <link rel="icon" href="Assets/images/favicon.png">
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid" style="background-color:${answers.color}">
+    <div class="container container-fluid">
+      <img src="" class="profileImg">
+      <p class="gitHubUserName"></p>
+      <a href=""><p class="location"></p></a>
+      <a href=""><p class="gitHubProfile"></p></a>
+      <a href=""><p class="blog"></p></a>
+      <p class="bio"></p>
+      <p class="repos"></p>
+      <p class="followers"></p>
+      <p class="stars"></p>
+      <p class="followees"></p>
+      <h1 class="display-4">Hi, I'm ${answers.name}</h1>
+      <h5>I am from ${answers.location}.</h5>
+      <h5>My favorite hobby is ${answers.hobby}.</h5>
+      <h5>My favorite game is ${answers.game}.</h5>
+      <h5>My favorite movie is ${answers.movie}.</h5>
+      <h4>Contact Info:</h4>
+      <ul class="list-group">
+        <li class="list-group-item">GitHub username: ${answers.github}</li>
+        <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+      </ul>
+    </div>
+  </div>
+</body>
+</html>`;
+
+//make the HTML file mobile-first
+}
+
+promptUser()
+  .then((answers) => {
+    const html = createHTML(answers);
+
+    return writeFileAsync("index.html", html);
+  })
+  .then(function() {
+    console.log("Successfully wrote to index.html");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
